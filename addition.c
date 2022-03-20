1eb55f9c02f7946f352ad3279c98264df746c300
SADEEQ ULLAH KHAN <101932287+sadeequllahkhan@users.noreply.github.com>
2022-03-20 11:05:09 -0700

    add.c
    addition.c

---
+++ addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+	int a;
+	int b;
+	printf("enter the value of first number\n");
+	scanf("%d",&a);
+	printf("enter the value of second number\n");
+	scanf("%d",&b);
+	int c=a+b;
+	printf("result is %d");
+	scanf("%d",&c);
+}
