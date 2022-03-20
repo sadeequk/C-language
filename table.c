e6f8e2679c270a013bccc2092744e2fbe242f35a
SADEEQ ULLAH KHAN <101932287+sadeequllahkhan@users.noreply.github.com>
2022-03-20 11:09:51 -0700

    Table.c
    Table in c language

---
+++ table.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main(){
+
+
+	int p;
+	printf("enter the values... \n");
+	scanf("%d",&p);
+	int a=4;
+	for(p=0;p<=15;p++){
+	
+	
+	printf("the result is :%d \n",p*a);
+}
+}
+
+		
+
+	
+
