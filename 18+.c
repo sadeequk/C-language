ee1bfc0cfbca44002672c70e58b33e0c5e394e76
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-01 18:45:36 +0500

    Add files via upload

---
+++ 18+.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ main(){
+	int age;
+	printf("enter the age of person...");
+	scanf("%d",&age);
+	if(age>=18)
+	printf("person is eligible for vote..");
+
+	else
+	printf("person is not eligible for vote..");
+	return 0;
+}
