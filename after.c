9bed9c098cf2df2719fa4fa785a143f7f3f8e06a
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-01 18:48:27 +0500

    Add files via upload

---
+++ after.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main(){
+	/*int f;
+	printf("enter the value of f....  \n");
+	scanf("%d",&f);
+	if(f==10){
+		printf("the result is ppdefined... \n");
+	}*/
+	       
+	       int age;
+	       printf("enter the age of person... \n");
+           scanf("%d",&age);
+		   if(age>=18)
+		   printf("eligible for vote.... \n");
+		   else
+		   printf("not eligible for vote... \n");	       
+	
+}
