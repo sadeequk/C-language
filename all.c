de404709c4afe79fe507d7e0756f18b822bc9806
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-02 20:48:49 +0500

    Add files via upload

---
+++ all.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+main(){
+	/*int a;
+	printf("enter the value of a  \n");
+	scanf("%d",&a);
+	int b;	
+	printf("enter the value of b  \n");
+	scanf("%d",&b);
+	int c=a+b;
+	printf("the result is :%d \n",c);*/
+	
+	 /*int a;
+	 printf("enter the value of a \n");
+	 scanf("%d",&a);
+	 if(a==2304)
+	 {
+	 	printf("deposit is possible...");
+	 	          
+	 }*/
+	        /* int password;
+	         printf("enter your password... \n");
+	         scanf("%d",&password);
+	         if(password<=100)
+	         {
+	         	printf("welcome to my new website... \n");
+			 }
+			 else
+			 {
+			 	printf("please press the correct password... \n");
+			 }*/
+			 /*int time;
+			 printf("any time... \n");
+	         scanf("%d",&time);
+	         if(time==2021)
+	         {
+	         	printf("corona era... \n");
+			 }
+			 else if(time==2020)
+			 {
+			 	printf("godd era... \n");
+			 }
+			 else if(time==2019)
+			 {
+			 	printf("very good era... \n");
+			 }
+			 else
+			 {
+			 	printf("previous era is outstanding... \n");
+			 }*/
+			/* int number;
+			 printf("enter any number... \n");
+			 scanf("%d",&number);
+			 int i;
+			 for(i=0;i<=20;i++)
+			 {
+			 	printf(" \n the table of 6 is :%d",i*number);
+			 	
+			 }*/
+			 /*int a,b,c;
+			 printf("enter your desired table... \n");
+			 scanf("%d",&a);
+		
+			 for(b=1;b<=10;b++)
+			 {
+			 	c=a*b;
+			printf("%d   *%   d= %d\n",a,b,c);
+			 }*/
+			 
