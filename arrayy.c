429389ace60d3614e1c5c4e51859a2896920c7ff
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-03 21:14:33 +0500

    Add files via upload

---
+++ arrayy.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+	int number[10]={10,20,30,40,50,60,70,80,90,100};
+	printf("array index is  :%d",number[0]);
+	int a=0;
+	while(a<10){
+		printf(" \n arrayy element  %d",number[a]);
+		a++;
+	}
+	
+	    
+		
+		
+		    
+}
