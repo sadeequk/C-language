a840de117b79252c20125028d564cc4454306daf
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-04 18:34:38 +0500

    Add files via upload

---
+++ bikhan.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+/*	printf("welcome to c language");*/
+	char message [20];
+	printf("enter any message... \n");
+	scanf("%s",&message);
+	printf("your message is :%s",message);
+	
+
+}
