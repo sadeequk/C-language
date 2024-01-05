23d65e5bfede0afbd3ca514c9d5ee729b3b712c0
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-05 20:32:42 +0500

    Add files via upload

---
+++ bolt.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+int main(){
+/*	char message[20];
+	printf("enter any message.. \n");
+	scanf("%s  \n",&message);
+	printf("your message is :%s  \n",message);*/
+	
+	    
+		/*int a;
+	    printf("enter the value of first number... \n");
+	    scanf("%d",&a);
+	    int b;
+	    printf("enter the value of second number...  \n");
+	    scanf("%d",&b);
+	    int c=a+b;
+	    printf("the result is :%d",c);*/
+	     
+	     
+	     
+	               /*int a=20;
+	               int p=10;
+	               int c=a*p;
+	               printf("the result is :%d",c);*/
+	
+	
+	     /*             int a=4;
+					  int p;    
+	  for(p=0;p<=15;p++){
+	  	printf("table of 4 is :%d   \n",p*a);
+	  }*/
+	                                              
+								               /*int password;
+												printf("enter your password... \n");
+												scanf("%d",&password);
+												if(password>=10)
+												printf("device is lock ....  \n");
+												else
+												printf("device is unlock...  \n");*/
+												
+																                 
+                                                                                
+																				/*int age;
+																				printf("enter the age...  \n");
+																				scanf("%d",&age);
+																				if(age>=18)
+																				printf("person is eligible for vote...  \n");*/	         
+	  
+	                                                                                             
+	                                                                                             
+                                                                                                  	
+	    
+	    
+	
+   
+	    
+	    
+	    
+	    
+	    
+	    
+}
