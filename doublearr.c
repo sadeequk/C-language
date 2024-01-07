d599dbeff8a1c99acacabd4cfe5a0b4398c0a62d
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-07 21:30:55 +0500

    Add files via upload

---
+++ doublearr.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+main(){
+	int arr[3][3];
+	int r,c;
+	 
+	 printf("enter the element of array....");
+	 for(r=0;r<3;r++)
+	 {
+	 	for(c=0;c<3;c++)
+	 	{
+	 		scanf("%d",&arr[r][c]);
+		 }
+	 
+	 }
+	 
+	 for(r=0;r<3;r++)
+	 {
+	 	for(c=0;c<3;c++){
+	 		printf("%d",arr[r][c]);
+		 }
+		 for(r=0;r<3;r++)
+		 {
+		 	for(c=0;c<3;c++){
+		 		
+		 		scanf("%d",&arr[r][c]);
+			 }
+	}
+	 	for(r=0;r<3;r++){
+		 
+	 	
+	 }
+		 
+		 
+	 		for(c=0;c<3;c++){
+				 printf("%d",arr[r][c]);
+	 		}
+			 
+		 }
+}
+
+
+
+
+
