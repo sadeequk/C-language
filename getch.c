bd1522e65f0a3aaac74c32cfc3bc1b52946cd1f0
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-12 23:42:51 +0500

    Add files via upload

---
+++ getch.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+            int marks;
+            printf("enter your marks...\n");
+            scanf("%d",&marks);
+            
+            if(marks=>50)
+            printf("the grade is A+...\n");
+            else
+            printf("the grade is a,b,c,d,e,f...\n");
+            getch();
+        }
+            
+        
+        
+            
