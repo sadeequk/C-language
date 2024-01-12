9c4065b6f11de6d020c729789ddd98db6020f425
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-12 23:43:04 +0500

    Add files via upload

---
+++ if (2).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ void main(){
+             int deposit;
+			 printf("enter the deposit amount...");
+			 scanf("%d",&deposit);
+			 if(deposit>=500)
+			 {
+			 	
+			 	
+				 
+				 printf("deposit is possible...\n");
+				 
+                				 
+				  } 
+				  	
+				  printf("main function is ended....\n");
+				  getch();
+			} 
+
