b60cdec05d47729f6b6000ed0dc294e8a73bd644
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-14 20:47:19 +0500

    Add files via upload

---
+++ ijk.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,k;
+	printf("%d",i);
+	printf("%d",j);
+	printf("%d",k);
+	getch();
+}
---
+++ loop.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main(){
+	int a=10;
+	int i=11;
+	
+	printf("%d",i*a);
+}
