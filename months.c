729b56d668a2a5bf12dd239d7022871575df7407
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-15 20:56:40 +0500

    Add files via upload

---
+++ months.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main(){
+	/*int months;
+	printf("enter the name of months... \n");
+	scanf("%d",&months);
+	if(months<=6){
+		printf("january,february,march,april,may,june... \n");
+	}
+	else
+	printf("july,august,september,october,november,december... \n");*/
+	int day;
+	printf("enter the name of days... \n");
+	scanf("%d",&day);
+	switch(day){
+		case1:
+			printf("sunday... \n");
+			break;
+			case2:
+	}
+}
