f07c97e707e2772e31e52a88d4cc51bc3d9d4e14
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-15 20:54:13 +0500

    Add files via upload

---
+++ malloc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int *p,*q;
+p=(int*)malloc(5*sizeof(int));
+	p[0]=2;
+	p[1]=4;
+	p[2]=6;
+	p[3]=8;
+	p[4]=10;
+	q=(int*)malloc(10*sizeof(int));.
+	printf("enter the values of p and q");
+	scanf("%d",&p,q);
+	
+}
