c4c84af71f32453c38ee97bb775f29ef49167305
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-20 22:33:56 +0500

    Add files via upload

---
+++ tool.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+	int number;
+	printf("any number... \n");
+	scanf("%d",&number);
+	int i;
+	for(i=0;i<=15;i++){
+		printf(" the table of 4 is:%d \n",i*number);
+		
+	}
+}
