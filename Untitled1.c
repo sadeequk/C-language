369e65da9e2f39434e10bc08506faf31fdcb0da6
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-20 22:32:19 +0500

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+	int a;
+	printf("enter the first  number... \n");
+	scanf("%d",&a);
+	int b;
+	printf("enter the second number... \n");
+	scanf("%d",&b);
+	int c=a+b;
+	printf("the result is:%d",c);
+}
