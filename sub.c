4ab92da097c1e655f4f3ddaa06b78e99e245923f
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-21 22:31:13 +0500

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+}
+subtraction(){
+	int a;
+	int b;
+	printf("enter your first number\n");
+	scanf("%d",&a);
+	printf("enter your second number\n");
+	scanf("%d",&b);
+	int result;
+	result=a-b;
+	printf("subtract is %d",result);
+	scanf("-============-");
+}
