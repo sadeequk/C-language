2cdf61ec70d6ada25aee9b73220fa9a2f9e36a6d
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-22 23:22:51 +0500

    Add files via upload

---
+++ simple.c
@@ -0,0 +1,114 @@
+#include<stdio.h>
+ void main(){
+//	printf("enter your name");
+ char message[20];
+printf("enter any message \n");
+scanf("%s",&message);
+printf("your message is :%s",message);
+/*int a=30;
+int b=20;
+int d=a+b;
+printf("%d \n",d);*/
+/*int a,b;
+printf("enter first number");
+scanf("%d",&a);
+printf("enter second number");
+scanf("%d",&b);
+printf("a and b is %d",(a+b));*/
+               
+              /*float lenth=20;
+               float width=40;
+               float rectangle=lenth*width;
+               printf("area %.1f",rectangle);*/
+                                                // assignment operator
+                                                float a=22.445;
+                                                printf("%.3f",a);
+                                                
+												/*int a=20;
+                                                int b=40;
+                                                b%=a;
+                                                printf("%d",b);*/
+                                                                        /*int age=20;
+                                                                        int salary=1000;
+                                                                        if(age==20 && salary==1000){
+                                                                        	printf("you can do");}
+                                                                        	else{
+																			
+																			
+                                                                        	printf("you can't do");
+                                                                        }*/
+                                                                        
+                                                                        	// days of week
+                                             /*                           	int day;
+                                                                        	printf("number of day");
+                                                                        	scanf("%d",&day);
+                                                                        	switch(day){
+                                                                        		case1:
+                                                                        			printf("sunday");
+                                                                        			break;
+                                                                        			case2:
+                                                                        				printf("monday");
+                                                                        				break;
+                                                                        				case3:
+                                                                        					printf("tuesday");
+                                                                        					break;
+                                                                        					case4:
+                                                                        						printf("wednesday");
+                                                                        						break;
+                                                                        						case5:
+                                                                        							printf("thursday");
+                                                                        							break;
+                                                                        							case6:
+                                                                        								printf("friday");
+                                                                        								break;
+                                                                        								case7:
+                                                                        									printf("saturday");
+                                                                        									break;
+                                                                        									default:
+                                                                        								printf("unknown...");}*/
+                                                                        	/*int number[6];
+																			number[0]=20;
+																			number[1]=30;
+																			number[2]=40;
+																			number[3]=50;
+																			number[4]=60;
+																			number[5]=70;*/
+																		//*	printf("%d",number[1]);
+																	/*	int i;							
+                                                                        for(i=0;i<6;i++)
+																		printf("element of array :%d \n",number[i]);*/	
+																		
+																										
+                                                                        	        /*int a;
+                                                                        	        printf("enter the odd number \n");
+                                                                        	        scanf("%d",&a);
+																					int b;
+																					printf("enter the even number \n");
+																					scanf("%d",&b);*/
+	                                                                                                           																				
+																	/*	int password;
+																		printf("enter your password...");
+																		scanf("%d",&password);
+																		if(password>=9)
+																		printf("the device is lock...");
+																		else
+																		printf("the device is unlock...");
+																		getch();*/	
+																		                /*int i,j;
+																						for(i=0;i<5;i++){
+																						}
+																						printf("%d \n",j);
+																						
+																						for(j=0;j<i;j++){
+																						}
+																						
+																						printf("%d \n",i); 
+																						getch();*/
+																					
+	}
+                                                                        		          							
+
+
+																		
+
+
