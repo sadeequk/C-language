d8df8733f7897e1647d3ce48cbddc09d37147e16
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-22 23:24:03 +0500

    Add files via upload

---
+++ rectangle.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+void main(){
+		int v,u;
+
+	for(v=0;v<=10;++v)         //  for descending order for(v=10;v>=0;--v)
+	                           //  for (u=1;u<=v;u++)
+	
+{	
+		{
+		for(u=0;u<=v;u++){
+				printf("1");	
+		}
+	}
+	printf("\n");
+}
+
+    /*int table;
+	printf("enter the value of number... \n");
+	scanf("%d",&table);
+	int a;
+	for(a=1;a<=table;a++){
+		printf(" \n table is :%d",a*table);
+	}*/
+	
+	           /*int table;
+	           printf("enter value of numbers... \n");
+	           scanf("%d",&table);
+	           int a;
+	           for(a=1;a<=table;a++){
+	           	printf(" \n the result is :%d",a*table);
+			   
+	           }*/
+			   
+			   
+	           
+	           
+			   
+         
+               
+            
+				   
+			   
+        
+}
