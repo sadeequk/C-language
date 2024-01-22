950cb45750d9bc6fc738769add0eb8519af01467
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-22 23:24:21 +0500

    Add files via upload

---
+++ pwe.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+main(){
+	int a;
+	int b;
+	printf("ENter value");
+	scanf("%d",a);
+	printf("ENter value");
+	scanf("%d",b);
+	int pow;
+	pow=a,b;
+	printf("Result is",pow);
+}
